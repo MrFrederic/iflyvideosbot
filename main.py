0b8f489b554157269cf04f50faa2b5e87b1c8e88
MrFrederic <stas123salo@gmail.com>
2024-05-31 00:43:59 +0200

    Logger level change

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 
 
 # Configure logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.ERROR)
 log = logging.getLogger(__name__)
 
 
